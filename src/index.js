1d9feb32594e8331804458733a1d0f89ae1048a3
AndreyOksenyuk <oksenyuk.andrey@gmail.com>
2020-08-02 21:54:12 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import store from './Redux/redux-store'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
-<BrowserRouter>
-   <Provider store={store}>
-      <App />
-   </Provider>
-</BrowserRouter>,
-document.getElementById('root')
+   <HashRouter>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </HashRouter>,
+   document.getElementById('root')
 )
 
 

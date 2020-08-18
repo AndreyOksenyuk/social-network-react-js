e981b064a5b2e4c547419651f22fb038aedefbfa
AndreyOksenyuk <oksenyuk.andrey@gmail.com>
2020-08-18 17:11:30 +0300

    link footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import style from './Footer.module.scss'
 
 const Footer = () => {
-   return ( 
+   return (
       <footer className={style.footer}>
          <p className={style.textFooter}>Oksenyuk Andrey | 2020</p>
+         <p className={style.textFooter}>Link to the github repository -  
+            <a 
+               href="https://github.com/AndreyOksenyuk/social-network-react-js"
+               target="_blank" rel="noreferrer noopener"
+            > https://github.com/AndreyOksenyuk/social-network-react-js
+            </a>
+         </p>
       </footer>
    )
 }
